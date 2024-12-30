11cef9272cd13f6928e703dd7d7e8db8eb8882be
rchllsosa <sosarachellea@gmail.com>
2024-12-31 04:03:39 +0800

    3rd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,6 @@ export default function Home() {
                 className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
                 onClick={() => {
                   setIsMoreModalOpen(false);
-                  setIsModalOpen(true);
                 }}
               >
                 Close
